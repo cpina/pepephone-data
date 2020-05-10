459d7f1982a49a568e0e9da1993310a41b87a61a
Carles Pina i Estany <carles@pina.cat>
2020-05-10 17:54:25 +0200

    Adds github action (take one)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pepephone-data',
-    version='1.0.2',
+    version='1.0.3',
     author='Carles Pina i Estany',
     author_email='carles@pina.cat',
     description='Shows Pepephone used and available data',
